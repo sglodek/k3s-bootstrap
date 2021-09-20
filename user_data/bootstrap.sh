f638293f0d20d01a62a1aae21643d19639dc863b
Spencer Glodek <sglodek@freelancer.com>
2021-09-19 17:19:28 -0700

    break up bootstrap script based on role

--- user_data/bootstrap.sh
+++ user_data/bootstrap.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+### Install Consul ###
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt update && apt install consul
+echo 'retry_join = ["provider=aws tag_key=role tag_value=consul_server"]' >> /etc/consul.d/consul.hcl
+systemctl start consul
+systemctl enable consul
+
+### Configure systemd-resolved to use Consul ###
+iptables -t nat -A OUTPUT -d localhost -p udp -m udp --dport 53 -j REDIRECT --to-ports 8600
+iptables -t nat -A OUTPUT -d localhost -p tcp -m tcp --dport 53 -j REDIRECT --to-ports 8600
+
+echo 'DNS=127.0.0.1' >> /etc/systemd/resolved.conf
+echo 'Domains=~consul' >> /etc/systemd/resolved.conf
+systemctl restart systemd-resolved.service
+
+### Install Role Specific Features ###
 git clone https://github.com/sglodek/k3s-bootstrap.git
-chmod 700 ./user_data/roles/base.sh
-./user_data/roles/base.sh
+cd k3s-bootstrap/user_data
+apt install awscli
+INSTANCE_ID=$(ec2metadata --instance-id)
+REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
+ROLE=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=role" --region=$REGION --output=text | cut -f5)
+
+source roles/$ROLE.sh
