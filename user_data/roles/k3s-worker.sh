9ed7a1d15246b04f37204a0432eadb4ea4095dba
Spencer Glodek <sglodek@freelancer.com>
2021-09-26 14:14:31 -0700

    bootstrap

--- user_data/roles/k3s-worker.sh
+++ user_data/roles/k3s-worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### Configure Consul ###
-echo 'retry_join = ["provider=aws tag_key=role tag_value=consul_server"]' >> /etc/consul.d/consul.hcl
+echo 'retry_join = ["provider=aws tag_key=role tag_value=consul-server"]' >> /etc/consul.d/consul.hcl
 systemctl start consul
 systemctl enable consul
 
