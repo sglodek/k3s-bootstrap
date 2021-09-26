61e2e57fe28ae86325b71c11452206cfeaff9d81
Spencer Glodek <sglodek@freelancer.com>
2021-09-26 13:37:33 -0700

    k3s script

--- user_data/roles/k3s-server.sh
+++ user_data/roles/k3s-server.sh
@@ -7,4 +7,4 @@ systemctl enable consul
 
 ### Install k3s ###
 #echo 'server = true' >> /etc/rancher/k3s/config.yaml
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -
+curl -sfL https://get.k3s.io | K3S_TOKEN="123" INSTALL_K3S_EXEC="server" sh -
--- user_data/roles/k3s-worker.sh
+++ user_data/roles/k3s-worker.sh
@@ -4,3 +4,7 @@
 echo 'retry_join = ["provider=aws tag_key=role tag_value=consul_server"]' >> /etc/consul.d/consul.hcl
 systemctl start consul
 systemctl enable consul
+
+### Install k3s ###
+#echo 'server = true' >> /etc/rancher/k3s/config.yaml
+curl -sfL https://get.k3s.io | K3S_TOKEN="123" K3S_URL=https://k3s-server.service.consul:6443 sh -
