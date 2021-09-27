757d4540d0f672dcc84334605a169df3f780eb1b
Spencer Glodek <sglodek@freelancer.com>
2021-09-26 19:46:06 -0700

    improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
-terraform1 plan -var='asg_count={"consul-server":3,"k3s-worker":0,"k3s-server":0}'
-terraform1 plan -var='asg_count={"consul-server":3,"k3s-worker":0,"k3s-server":1}' -var='k3s_bootstrap=true'
-terraform1 plan -var='asg_count={"consul-server":3,"k3s-worker":0,"k3s-server":3}'
-terraform1 plan -var='asg_count={"consul-server":3,"k3s-worker":3,"k3s-server":3}'
+terraform1 apply -auto-approve -var='asg_count={"consul-server":3,"k3s-worker":0,"k3s-server":0}'
+sleep 60
+terraform1 apply -auto-approve -var='asg_count={"consul-server":3,"k3s-worker":0,"k3s-server":1}' -var='k3s_bootstrap=true'
+sleep 60
+terraform1 apply -auto-approve -var='asg_count={"consul-server":3,"k3s-worker":0,"k3s-server":3}'
+sleep 60
+terraform1 apply -auto-approve -var='asg_count={"consul-server":3,"k3s-worker":3,"k3s-server":3}'
