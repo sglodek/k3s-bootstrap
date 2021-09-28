9d94d50c1d32fc7c66435471a0305f8564e2c4f2
Spencer Glodek <sglodek@freelancer.com>
2021-09-27 17:07:34 -0700

    fix k3s-worker bootstrap

--- user_data/roles/k3s-worker.sh
+++ user_data/roles/k3s-worker.sh
@@ -6,4 +6,4 @@ echo "bind_addr = \"$IPV4_ADDR\"" >> /etc/consul.d/consul.hcl
 systemctl restart consul
 
 ### Install k3s ###
-curl -sfL https://get.k3s.io | K3S_TOKEN="123" K3S_URL="https://k3s-server.consul.service:6443" INSTALL_K3S_EXEC="agent" sh -
+curl -sfL https://get.k3s.io | K3S_TOKEN="123" K3S_URL="https://k3s-server.service.consul:6443" INSTALL_K3S_EXEC="agent" sh -
